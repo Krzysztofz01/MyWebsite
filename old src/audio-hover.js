142cfbaaa13d563b846d6e9500cdea9c9910e694
Krzychu <krzysztof.zon2001@gmail.com>
2019-03-02 11:28:41 +0100

    Duże poprawki w index, gotowa galeria oraz muzyka

---
+++ old src/audio-hover.js
@@ -0,0 +1,5 @@
+var creosote = new Audio();
+var pov = new Audio();
+
+creosote.src = "../content/music/creosote.mp3";
+pov.src = "../content/music/pov.mp3";
---
+++ src/audio.js
@@ -0,0 +1,5 @@
+var creosote = new Audio();
+creosote.src = "content/music/creosote.mp3"
+
+var pov = new Audio();
+pov.src = "content/music/pov.mp3"
