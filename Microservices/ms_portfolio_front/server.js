d666138e8c88ed735d2f364a9e722526235806bd
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2020-08-18 14:24:16 +0200

    Gallery

--- Microservices/ms_portfolio_front/server.js
+++ Microservices/ms_portfolio_front/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const serveIndex = require('serve-index');
 const app = express();
 require('dotenv').config();
 
+//Static files middleware
 app.use(express.static('public', {
     dotfiles: "ignore",
     index: false,
@@ -11,8 +13,18 @@ app.use(express.static('public', {
     }
 }));
 
-app.get('*', (req, res) => {
+//Enable dir indexing for gallery images
+app.use('/galleryImages', serveIndex(__dirname + '/public/galleryImages'));
+
+//Index route
+app.get('/', (req, res) => {
     res.sendFile('./public/home.html', { root: __dirname });
 });
 
+//Gallery route
+app.get('/gallery', (req, res) => {
+    res.sendFile('./public/gallery.html', { root: __dirname });
+});
+
+//Server listen on port defined in .env file
 app.listen(process.env.APP_PORT);
